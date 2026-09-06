23c124646a48a966117f0969fc0e34c7d21fcd4f
mimranfaruqi <44291943+mimranfaruqi@users.noreply.github.com>
2026-09-06 06:29:39 +0200

    Align Docker runner plugins with 29.8.0 image

--- runner-images/docker/smoke.sh
+++ runner-images/docker/smoke.sh
@@ -6,9 +6,9 @@ test -x /home/runner/run.sh
 bash -n /home/runner/run.sh
 
 docker --version | grep -F 'Docker version 29.8.0'
-docker buildx version | grep -F 'v0.36.1'
-docker compose version | grep -F 'v5.4.0'
-docker-compose version | grep -F 'v5.4.0'
+docker buildx version | grep -F 'v0.37.0'
+docker compose version | grep -F 'v5.5.1'
+docker-compose version | grep -F 'v5.5.1'
 
 for forbidden in dockerd containerd ctr runc docker-proxy docker-init; do
   ! command -v "${forbidden}"
